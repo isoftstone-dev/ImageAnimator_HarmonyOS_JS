f1f9e57cdea2528e4919e8e0020638e56079ff58
wangfan <2391517396@qq.com>
2020-12-31 11:46:53 +0800

    入场动画组件

---
+++ entry/src/main/java/com/llew/font/slide/imageanimator/MyApplication.java
@@ -0,0 +1,10 @@
+package com.llew.font.slide.imageanimator;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
